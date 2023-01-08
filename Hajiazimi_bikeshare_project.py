cec192eba7edd62e0a8d0ebca6015f635ddc5e47
Moneer Hajiazimi <moneerelizabeth@gmail.com>
2023-01-08 16:50:15 -0500

    Clarify dependencies of code by moving main function from the bottom to the top of the code.

--- Hajiazimi_bikeshare_project.py
+++ Hajiazimi_bikeshare_project.py
@@ -12,6 +12,22 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+def main(): 
+    """ Contains all functions needed to fully execute bikeshare code. """
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        raw_data(df)
+
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -212,21 +228,6 @@ def raw_data(df):
             print('Done!')
             break
 
-def main(): 
-    while True:
-        city, month, day = get_filters()
-        df = load_data(city, month, day)
-
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
-        raw_data(df)
-
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
-            break
-
-
+# call main() to execute code
 if __name__ == "__main__":
 	main()
